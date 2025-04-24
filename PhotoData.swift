46b34e544a60b44ad941c6d2ba62bbfaca1dc6ca
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-24 11:51:45 +0900

    記述追加

--- PhotoData.swift
+++ PhotoData.swift
@@ -8,10 +8,10 @@ struct PhotoData: Identifiable {
 }
 // 構造体PhotoData の写真データが入った配列を作成
 var PhotoArray = {
-    PhotoData(imageName:"", title:""),
-    PhotoData(imageName:"", title:""),
-    PhotoData(imageName:"", title:""),
-    PhotoData(imageName:"", title:""),
-    PhotoData(imageName:"", title:""),
-    PhotoData(imageName:"", title:""),
+    PhotoData(imageName:"IMG_042440.png", title:"幻想的な風景"),
+    PhotoData(imageName:"IMG_042441.png", title:"めっちゃ幻想的な風景"),
+    PhotoData(imageName:"IMG_042442.png", title:"すんげー幻想的な風景"),
+    PhotoData(imageName:"IMG_042443.png", title:"とっても幻想的な風景"),
+    PhotoData(imageName:"IMG_042444.png", title:"どえらい幻想的な風景"),
+    PhotoData(imageName:"IMG_042445.png", title:"ホントに幻想的な風景"),
 }
